e00178e832207d7e425dccf0979c9378e59eca8d
Daimon <118897859+Daimon153@users.noreply.github.com>
2022-11-25 17:36:01 +0500

    Added cupboards

---
+++ Assets/scripts/DoorSkaf.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSkaf :MonoBehaviour, IInteractable
+{
+	public bool _isOpened;
+public Animator _animator;
+
+
+private void Start()
+{
+	_animator = GetComponent<Animator>();
+}
+public string GetDescription()
+{
+	if (_isOpened)
+	{
+		return "Нажми E, чтобы закрыть шкаф";
+
+	}
+	return "Нажми E, чтобы открыть шкаф";
+}
+public void Interact()
+{
+		_isOpened = !_isOpened;
+
+	if (_isOpened)
+	{
+		_animator.SetBool("_isOpened", true);
+	}
+
+	else
+	{
+		_animator.SetBool("_isOpened", false);
+	}
+}
+}
